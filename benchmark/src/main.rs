5a80a5042e01fa8a6be3ad6cfd38c228d24f0d43
Isaac Elliott <isaace71295@gmail.com>
2020-05-06 07:56:51 +1000

    got a loopy eval going (CEK)

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,92 +1,108 @@
 use ast::de_bruijn;
 use ast::syntax;
-use eval::eval;
 use eval::heap::Heap;
 use eval::stack::Stack;
+use eval::{eval, eval_loop};
 use lexer::Lexer;
 use parser::Parser;
-use pretty;
 use span::SourceFiles;
 use std::path::Path;
+
+fn build_eval_expr<'builder, 'value>(
+    builder: &'builder de_bruijn::ExprBuilder<'value>,
+) -> de_bruijn::ExprRef<'value>
+where
+    'builder: 'value,
+{
+    let nil =
+        // \n ->
+        builder.mk_lam(
+            // \c ->
+            builder.mk_lam(
+                // n
+                builder.mk_var(1),
+            ),
+        );
+    let cons =
+        // \a ->
+        builder.mk_lam(
+            // \b ->
+            builder.mk_lam(
+                // \n ->
+                builder.mk_lam(
+                    // \c ->
+                    builder.mk_lam(
+                        builder.mk_app(
+                            // c a
+                            builder.mk_app(builder.mk_var(0), builder.mk_var(3)),
+                            builder.mk_app(
+                                // b n
+                                builder.mk_app(builder.mk_var(2), builder.mk_var(1)),
+                                // c
+                                builder.mk_var(0)
+                            )
+                        ),
+                    ),
+                ),
+            ),
+        );
+    let zero_to_5 = builder.mk_app(
+        // cons 0
+        builder.mk_app(cons, builder.mk_u64(0)),
+        builder.mk_app(
+            // cons 1
+            builder.mk_app(cons, builder.mk_u64(1)),
+            builder.mk_app(
+                // cons 2
+                builder.mk_app(cons, builder.mk_u64(2)),
+                builder.mk_app(
+                    // cons 3
+                    builder.mk_app(cons, builder.mk_u64(3)),
+                    builder.mk_app(
+                        // cons 4
+                        builder.mk_app(cons, builder.mk_u64(4)),
+                        builder.mk_app(
+                            // cons 5
+                            builder.mk_app(cons, builder.mk_u64(5)),
+                            //nil
+                            nil,
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    );
+    builder.mk_app(
+        // zero_to_5 0
+        builder.mk_app(zero_to_5, builder.mk_u64(0)),
+        // \a ->
+        builder.mk_lam(
+            // \b ->
+            builder.mk_lam(
+                // a + b
+                builder.mk_addu64(builder.mk_var(1), builder.mk_var(0)),
+            ),
+        ),
+    )
+}
 
 fn run() -> bool {
     let args: Vec<String> = std::env::args().into_iter().collect();
     match args[1].as_str() {
         "eval" => {
             let builder = de_bruijn::ExprBuilder::new();
-            let nil =
-                // \n ->
-                builder.mk_lam(
-                    // \c ->
-                    builder.mk_lam(
-                        // n
-                        builder.mk_var(1),
-                    ),
-                );
-            let cons =
-                // \a ->
-                builder.mk_lam(
-                    // \b ->
-                    builder.mk_lam(
-                        // \n ->
-                        builder.mk_lam(
-                            // \c ->
-                            builder.mk_lam(
-                                builder.mk_app(
-                                    // c a
-                                    builder.mk_app(builder.mk_var(0), builder.mk_var(3)),
-                                    builder.mk_app(
-                                        // b n
-                                        builder.mk_app(builder.mk_var(2), builder.mk_var(1)),
-                                        // c
-                                        builder.mk_var(0)
-                                    )
-                                ),
-                            ),
-                        ),
-                    ),
-                );
-            let zero_to_5 = builder.mk_app(
-                // cons 0
-                builder.mk_app(cons, builder.mk_u64(0)),
-                builder.mk_app(
-                    // cons 1
-                    builder.mk_app(cons, builder.mk_u64(1)),
-                    builder.mk_app(
-                        // cons 2
-                        builder.mk_app(cons, builder.mk_u64(2)),
-                        builder.mk_app(
-                            // cons 3
-                            builder.mk_app(cons, builder.mk_u64(3)),
-                            builder.mk_app(
-                                // cons 4
-                                builder.mk_app(cons, builder.mk_u64(4)),
-                                builder.mk_app(
-                                    // cons 5
-                                    builder.mk_app(cons, builder.mk_u64(5)),
-                                    //nil
-                                    nil,
-                                ),
-                            ),
-                        ),
-                    ),
-                ),
-            );
-            let expr = builder.mk_app(
-                // zero_to_5 0
-                builder.mk_app(zero_to_5, builder.mk_u64(0)),
-                // \a ->
-                builder.mk_lam(
-                    // \b ->
-                    builder.mk_lam(
-                        // a + b
-                        builder.mk_addu64(builder.mk_var(1), builder.mk_var(0)),
-                    ),
-                ),
-            );
+            let expr = build_eval_expr(&builder);
             for _ in 0..450000 {
                 let heap = Heap::with_capacity(1024);
                 let _ = eval(&heap, &Vec::new(), expr);
+            }
+        }
+        "eval_loop" => {
+            let builder = de_bruijn::ExprBuilder::new();
+            let expr = build_eval_expr(&builder);
+            for _ in 0..450000 {
+                let heap = Heap::with_capacity(1024);
+                let _ = eval_loop(&heap, expr);
             }
         }
         "parse" => {
--- eval/src/lib.rs
+++ eval/src/lib.rs
@@ -53,6 +53,112 @@ where
     res
 }
 
+type Env<'expr, 'value> = Vec<&'value Value<'expr, 'value>>;
+type ValueRef<'expr, 'value> = &'value Value<'expr, 'value>;
+
+#[derive(Debug)]
+enum Hole {
+    Hole,
+}
+
+/// The meaning of `Cont` is a function from `ValueRef -> ValueRef`
+#[derive(Debug)]
+enum Cont<'expr, 'value> {
+    AppL(Env<'expr, 'value>, Hole, ExprRef<'expr>),
+    AppR(Env<'expr, 'value>, ExprRef<'expr>, Hole),
+    AddU64L(Env<'expr, 'value>, Hole, ExprRef<'expr>),
+    AddU64R(u64, Hole),
+}
+
+#[derive(Debug)]
+enum Code<'expr, 'value> {
+    Input(ExprRef<'expr>),
+    Output(ValueRef<'expr, 'value>),
+}
+
+pub fn eval_loop<'expr, 'heap, 'value>(
+    heap: &'heap Heap<'expr, 'value>,
+    expr: ExprRef<'expr>,
+) -> ValueRef<'expr, 'value>
+where
+    'heap: 'value,
+{
+    use crate::Code::*;
+    use crate::Cont::*;
+    use crate::Hole::*;
+
+    let mut env: Env<'expr, 'value> = Vec::new();
+    let mut code: Code<'expr, 'value> = Input(expr);
+    let mut cont: Vec<Cont<'expr, 'value>> = Vec::new();
+    loop {
+        // println!("C: {:?}", code);
+        // println!("E: {:?}", env);
+        // println!("K: {:?}", cont);
+        // println!("---------------------------------");
+        match code {
+            Input(expr) => match expr {
+                Expr::U64(n) => {
+                    code = Output(heap.alloc(Value::U64(*n)));
+                }
+                Expr::Var(n) => {
+                    code = Output(env[env.len() - n - 1]);
+                }
+                Expr::App(l, r) => {
+                    code = Input(l);
+                    cont.push(AppL(env.clone(), Hole, r));
+                }
+                Expr::Lam(body) => {
+                    code = Output(heap.alloc(Value::Closure {
+                        env: env.clone(),
+                        body: body,
+                    }));
+                }
+                Expr::AddU64(l, r) => {
+                    code = Input(l);
+                    cont.push(AddU64L(env.clone(), Hole, r));
+                }
+            },
+            Output(value) => match cont.pop() {
+                Option::None => match code {
+                    Input(_) => panic!("eval_loop failed: no output to return"),
+                    Output(value) => {
+                        return value;
+                    }
+                },
+                Option::Some(c) => match c {
+                    AppL(r_env, Hole, r) => match value {
+                        Value::Closure { env: l_env, body } => {
+                            code = Input(r);
+                            env = r_env;
+                            cont.push(AppR(l_env.clone(), body, Hole));
+                        }
+                        _ => panic!("eval_loop failed: Expected closure, got {:?}", value),
+                    },
+                    AppR(next_env, body, Hole) => {
+                        env = next_env.clone();
+                        env.push(value);
+                        code = Input(body);
+                    }
+                    AddU64L(r_env, Hole, r) => match value {
+                        Value::U64(l) => {
+                            code = Input(r);
+                            env = r_env;
+                            cont.push(AddU64R(*l, Hole));
+                        }
+                        _ => panic!("eval_loop failed: Expected u64, got {:?}", value),
+                    },
+                    AddU64R(l, Hole) => match value {
+                        Value::U64(r) => {
+                            code = Output(heap.alloc(Value::U64(l + r)));
+                        }
+                        _ => panic!("eval_loop failed: Expected u64, got {:?}", value),
+                    },
+                },
+            },
+        }
+    }
+}
+
 #[test]
 fn test_eval1() {
     let input = &Expr::Lam(&Expr::Var(0));
@@ -128,4 +234,81 @@ fn test_eval6() {
     let output = &Value::U64(16);
     let mut heap = Heap::with_capacity(1024);
     assert_eq!(eval(&mut heap, &Vec::new(), input), output)
+}
+
+#[test]
+fn test_eval_loop1() {
+    let input = &Expr::Lam(&Expr::Var(0));
+    let output = &Value::Closure {
+        env: Vec::new(),
+        body: &Expr::Var(0),
+    };
+    let mut heap = Heap::with_capacity(1024);
+    assert_eq!(eval_loop(&mut heap, input), output)
+}
+
+#[test]
+fn test_eval_loop2() {
+    let id = &Expr::Lam(&Expr::Var(0));
+    let input = &Expr::App(id, id);
+    let output = &Value::Closure {
+        env: Vec::new(),
+        body: &Expr::Var(0),
+    };
+    let mut heap = Heap::with_capacity(1024);
+    assert_eq!(eval_loop(&mut heap, input), output)
+}
+
+#[test]
+fn test_eval_loop3() {
+    let id = &Expr::Lam(&Expr::Var(0));
+    let id_value = &Value::Closure {
+        env: Vec::new(),
+        body: &Expr::Var(0),
+    };
+    let konst = &Expr::Lam(&Expr::Lam(&Expr::Var(1)));
+    let input = &Expr::App(konst, id);
+    let output = &Value::Closure {
+        env: vec![id_value],
+        body: &Expr::Var(1),
+    };
+    let mut heap = Heap::with_capacity(1024);
+    assert_eq!(eval_loop(&mut heap, input), output)
+}
+
+#[test]
+fn test_eval_loop4() {
+    let id = &Expr::Lam(&Expr::Var(0));
+    let konst = &Expr::Lam(&Expr::Lam(&Expr::Var(1)));
+    let konst_id = &Expr::App(konst, id);
+    let input = &Expr::App(konst_id, konst);
+    let output = &Value::Closure {
+        env: Vec::new(),
+        body: &Expr::Var(0),
+    };
+    let mut heap = Heap::with_capacity(1024);
+    assert_eq!(eval_loop(&mut heap, input), output)
+}
+
+#[test]
+fn test_eval_loop5() {
+    let plus = &Expr::Lam(&Expr::Lam(&Expr::AddU64(&Expr::Var(0), &Expr::Var(1))));
+    let plus_9 = &Expr::App(plus, &Expr::U64(9));
+    let input = &Expr::App(plus_9, &Expr::U64(7));
+    let output = &Value::U64(16);
+    let mut heap = Heap::with_capacity(1024);
+    assert_eq!(eval_loop(&mut heap, input), output)
+}
+
+#[test]
+fn test_eval_loop6() {
+    let plus = &Expr::Lam(&Expr::Lam(&Expr::AddU64(&Expr::Var(0), &Expr::Var(1))));
+    let apply_9_7 = &Expr::Lam(&Expr::App(
+        &Expr::App(&Expr::Var(0), &Expr::U64(9)),
+        &Expr::U64(7),
+    ));
+    let input = &Expr::App(apply_9_7, plus);
+    let output = &Value::U64(16);
+    let mut heap = Heap::with_capacity(1024);
+    assert_eq!(eval_loop(&mut heap, input), output)
 }
